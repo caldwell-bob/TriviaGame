9fae270df221078490dc81f7d95cee95bffddf41
Bob Caldwell <themaccat@gmail.com>
2019-10-07 18:34:37 -0400

    tweaking iterating though the array..save point 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,16 +11,15 @@ var trivia = {
 
   // TODO determimne best way to handle countdown timer
 
-  
   initQuestionBank: function() {
     (questionBank = [
       {
-        question: "What is the name of the only female singer in the band?",
-        answer: "Donna",
+        question: "What is the name of the only animal in the band?",
+        answer: "Pig Pen",
         selection_0: "Jill",
         selection_1: "Donna",
         selection_2: "Brandy",
-        selection_3: "Lana K"
+        selection_3: "Pig Pen"
       },
       {
         question: "What is the name of the only female singer in the band?",
@@ -32,33 +31,54 @@ var trivia = {
       }
     ]),
       console.log("Number of Questions: " + questionBank.length);
-      return questionBank
+    return questionBank;
   },
 
   updateDisplay: function() {},
 
   nextQuestion: function(questionObj) {
-      console.log("inside of nextQuestion");
-      // TODO update Countdown Timer and write to Div
-      // TODO Display next question in array
-      // TODO Display the new selections
-    //   $("#remainingSecs").text = "Remaining Seconds: <need to add functionality";
-    // trivia.remainingDiv.text("Remaining Seconds: <need to add functionality");
-    trivia.remainingDiv.innerHTML = ("Remaining Seconds: <need to add functionality");
-    trivia.questionDiv.innerHTML = ("Question: " + questionObj.question);
-    trivia.selectionDiv_0.innerHTML = (questionObj.selection_0);
-    trivia.selectionDiv_1.innerHTML = (questionObj.selection_1);
-    trivia.selectionDiv_2.innerHTML = (questionObj.selection_2);
-    trivia.selectionDiv_3.innerHTML = (questionObj.selection_3);
+    console.log("inside of nextQuestion");
     console.log(questionObj);
 
+    // TODO update Countdown Timer and write to Div
+    // TODO Display next question in array
+    // TODO Display the new selections
+    //   $("#remainingSecs").text = "Remaining Seconds: <need to add functionality";
+    // trivia.remainingDiv.text("Remaining Seconds: <need to add functionality");
+    trivia.remainingDiv.innerHTML =
+      "Remaining Seconds: <need to add functionality";
+    trivia.questionDiv.innerHTML = "Question: " + questionObj.question;
+    trivia.selectionDiv_0.innerHTML = questionObj.selection_0;
+    trivia.selectionDiv_1.innerHTML = questionObj.selection_1;
+    trivia.selectionDiv_2.innerHTML = questionObj.selection_2;
+    trivia.selectionDiv_3.innerHTML = questionObj.selection_3;
+
+ 
+
+    console.log("leaving nextQuestion");
   },
 
   playTrivia: function() {
+    let selected = false;
     console.log("Inside playTrivia");
     questionBank = this.initQuestionBank();
-    // console.log(questionBank[1].answer);
-    trivia.nextQuestion(questionBank[0]);
+    console.log(questionBank);
+  
+    for (i = 0; i < questionBank.length; i++) {
+      console.log("tresting " + "I is : "+ i + questionBank[i]);
+
+      trivia.nextQuestion(questionBank[i]);
+      break;
+      console.log("Why am I out of nextQuestion with nothing displayed yet?");
+
+      $(".selectOption").click(function() {
+       
+        console.log("selected-----");
+        selected = true;
+      });
+
+      console.log(i);
+    }
   }
 };
 
