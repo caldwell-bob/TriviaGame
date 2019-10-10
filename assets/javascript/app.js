360080f687dbd1ff72c8c87c82a0a42de2a182ee
Bob Caldwell <themaccat@gmail.com>
2019-10-09 20:11:05 -0400

    added final display page logic, resetting time to 30

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,8 +9,8 @@ var app = {
   imgDiv: document.getElementById(""),
   correctDiv: document.getElementById("correct"),
 
-  // TODO determimne best way to handle countdown timer
-  // ! correct -> 0:True, 1:False, 2:NULL
+  
+  // TODO (low) - remove "correct" field from object
   initQuestionBank: function() {
     (questionBank = [
       {
@@ -58,10 +58,10 @@ var app = {
     return questionBank;
   },
 
-  updateDisplayGameOver: function(good, bad) {
+  updateDisplayGameOver: function(good, bad, noTime) {
     console.log("inside updateDisplayGameOver");
     $("#outcome").show();
-    $(".selectOption").hide();
+    // $(".selectOption").hide();
     $("#remainingSecs").hide();
     $("#question").hide();
     // $("#outcome").hide();
@@ -69,6 +69,15 @@ var app = {
     console.log("end of array, need to create Final Results Display");
     console.log("Correct: " + good);
     console.log("Incorrect: " + bad);
+    console.log("Ran out of Time: " + noTime);
+
+    app.selectionDiv_0.innerHTML = "Correct Guesses: " + good;
+    app.selectionDiv_1.innerHTML = "Incorrect Guesses: " + bad;
+    app.selectionDiv_2.innerHTML = "Ran Out of Time: " + noTime;
+    app.selectionDiv_3.innerHTML = "Would you like to play again?";
+    // $("#selection_3").click(function() {
+
+    // },
 
     console.log("leaving updateDisplayGameOver");
   },
@@ -77,7 +86,6 @@ var app = {
     console.log("inside updateOutOfTime");
     $("#outcome").show();
     $(".selectOption").hide();
-    // $("#outcome").hide();
     app.outcomeDiv.innerHTML = "You done ran out of time son!";
     $("#outcome").hide(3000, function() {
       // $("#outcome").show();
@@ -92,16 +100,13 @@ var app = {
     // $("#outcome").hide();
     app.outcomeDiv.innerHTML = "Winner Winner Chicken Dinner!!";
     $("#outcome").hide(3000, function() {
-      // $("#outcome").show();
+      
     });
     console.log("leaving updateDisplayWinner");
   },
 
   updateDisplayLoser: function(questionObj) {
     console.log("inside updateDisplayLoser");
-    // TODO create result divs
-    // TODO write to result divs
-
     $("#outcome").show();
     $(".selectOption").hide();
     // $("#outcome").hide();
@@ -116,75 +121,72 @@ var app = {
     console.log("inside of updateDisplay");
     console.log("obj.id = " + questionObj.id);
     console.log(questionObj);
-    // $("#outcome").hide();
     $(".selectOption").show();
-    // * lets clear the divs first off
-    // $("#question").empty()
-
-    // app.remainingDiv.innerHTML = "Remaining Seconds: need to add functionality";
+    app.remainingDiv.innerHTML = " Remaining Seconds: 30";
     app.questionDiv.innerHTML = "Question: " + questionObj.question;
-    // $("#question").innerHTML = "Question: " + questionObj.question;
-
     app.selectionDiv_0.innerHTML = questionObj.selection_0;
     app.selectionDiv_1.innerHTML = questionObj.selection_1;
     app.selectionDiv_2.innerHTML = questionObj.selection_2;
     app.selectionDiv_3.innerHTML = questionObj.selection_3;
-
-    // TODO update Countdown Timer and write to Div
-
     console.log("leaving updateDisplay");
   },
 
   playTrivia: function() {
-    let x = 0;
+    let x = 0;  // used as counter for  click events, which is how we iterate through data array
     let correctGuess = 0;
     let incorrectGuess = 0;
     let ranOutOfTime = 0;
 
-    var number = 30;
+    var number = 30; // Used as timer countdown
     var intervalId;
 
     function upDateTimer(){
       console.log("in upDateTimer" + number);
       number--;
-      app.remainingDiv.innerHTML = number;
+      app.remainingDiv.innerHTML = " Remaining Seconds: " + number;
       if (number === 0){
         // alert("You done ran out of time son.");
         clearInterval(intervalId);
         ranOutOfTime +=1;
         app.updateOutOfTime();
         if (x < arrayLength) {
+          // TODO Get Timer to Reset after running out of town, correct guess, incorrect guess
+          // ? Am I dealing with a scoping issue here
           number = 30;
           app.updateDisplay(questionBank[x]);
-          console.log("in if x < arraryLength, just back from updateDisplay");
+          console.log("in if x < arraryLength, in upDateTimer, just back from updateDisplay");
         }
       }
 
     };
 
     function getSelection() {
-     
+      console.log("in getSelection");
+      // * lets clear out the intervalID 
       clearInterval(intervalId);
-     
+      // * use setInterval to call upDateTimer every sec, to update remainSseconds Div
+      number = 30; // Timer runs for this many seconds
       intervalId = setInterval(upDateTimer, 1000);
 
+      // * lets set a click event on selectOption divs
       $(".selectOption").click(function() {
        
         console.log(this);
         console.log(this.innerHTML);
-        // $("#remainingSecs").html(number);
-
         // * Check if correct answer
         if (this.innerHTML === questionBank[x].answer) {
+          clearInterval(intervalId);
           console.log("You guessed correctly, rock on!");
-          correctGuess += 1;
+          correctGuess += 1; // * increase correctGuess total
           app.updateDisplayWinner();
           if (x < arrayLength) {
+            clearInterval(intervalId);
             app.updateDisplay(questionBank[x]);
             console.log("in if x < arraryLength, just back from updateDisplay");
           }
           // * If incorrect answer
         } else {
+          clearInterval(intervalId);
           app.updateDisplayLoser();
           console.log("Nopers, that wasn't it...");
           incorrectGuess += 1;
@@ -199,18 +201,24 @@ var app = {
           questionBank[x].correct = 2;
           app.updateDisplay(questionBank[x]);
         } else {
-          app.updateDisplayGameOver(correctGuess, incorrectGuess);
+          app.updateDisplayGameOver(correctGuess, incorrectGuess, ranOutOfTime);
         }
       });
     }
-// * 
+
+    // TODO - Get the timer to restart after an event (outOfTime, correct/incorrectGuess)
+
+    // * Start of logic for playTrivia -----------------------------------------------
     console.log("Inside playTrivia");
+    // initate array w questions/answers by calling initQuestionBank method
     questionBank = this.initQuestionBank();
+    // grabbing the number of sets of questions/answers in array
     let arrayLength = questionBank.length;
     console.log(questionBank);
     console.log("x = " + x);
+    // updating UI w questions and answers from current object in array
     app.updateDisplay(questionBank[x]);
-
+    // Call getSelection to set our on click function into action
     getSelection();
    
   }
